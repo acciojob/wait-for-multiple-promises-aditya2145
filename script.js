c8674b146199ca4b958a124c7d6be1370350e13f
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-15 20:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const randomPromise = new Promise((resolve, reject) => {
+	const time = Math.random() * 2 + 1;
+	setTimeout(() => {
+		resolve(time.toFixed(3));
+	}, time*1000)
+});
+
+async function populateTable() {
+	const promises = [randomPromise, randomPromise, randomPromise];
+	const result = await Promise.all(promises);
+
+	const tbody = document.getElementById('output');
+	tbody.innerHTML = '';
+
+	result.forEach((time, index) => {
+		const promiseName = `Promise ${index + 1}`;
+		const row = `<tr>
+						<td>${promiseName}</td>
+						<td>${time}</td>
+					</tr>`;
+		tbody.insertAdjacentHTML('beforeend', row)
+	});
+
+	const totalTime = result.reduce((acc, curr) => acc + parseFloat(curr), 0).toFixed(3);
+	const totalRow = `<tr>
+						<td>Total</td>
+						<td>${totalTime}</td>
+	                  </tr>`;
+	tbody.insertAdjacentHTML('beforeend', totalRow);
+}
+populateTable();
+
